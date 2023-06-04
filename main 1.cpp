cc8b212f79bc82c1c53fa1366e1635e3ca8fab17
RyoEkka <128150504+ryoekka@users.noreply.github.com>
2023-06-04 22:03:53 +0700

    main 1.cpp

--- main 1.cpp
+++ main 1.cpp
@@ -25,51 +25,11 @@ void cari_genap_terkecil(const vector<int>& deret){
     }
 }
 
-void bubbleshort(vector<int>& deret){
-    int n = deret.size();
-    for (int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(deret[j] < deret[j+1]){
-                swap(deret[j], deret[j+1]);
-            }
-        }
-    }
-}
-void selectionsort(vector<int>&deret){
-    int n= deret.size();
-    for (int i=0; i<n-1; i++){
-        int maksindex=i;
-        for(int j=i+1; j<n; j++){
-            if(deret[j]>deret[maksindex]){
-                maksindex=j;
-            }
-        }
-        swap(deret[maksindex], deret[i]);
-    }
-}
-
 int main()
 {
     vector<int> deret = {6,7,4,3,2};
 
     cari_genap_terkecil(deret);
-
-    vector<int> deret {5,4,3,2,2,1,1};
-
-    bubbleshort(deret);
-    cout << "hasil bubblesort : ";
-    for(int nilai: deret){
-        cout << nilai << " ";
-    }
-    cout <<  endl;
-
-    deret = {5,4,3,2,2,1,1};
-    selectionsort(deret);
-    cout << "hasil selection sort : ";
-     for(int nilai: deret){
-        cout << nilai << " ";
-    }
-     cout <<  endl;
 
     return 0;
 }
